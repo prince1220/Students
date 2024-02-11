96f448b774d90709451caa31c357b6e029af02d3
prince1220 <frontdeskdesigns@gamil.com>
2024-02-11 16:52:12 -0500

    Add event to table

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 ///we have form with id="input-form"
 const formEl = document.getElementById("input-form");
+const tableEl = document.getElementById("student-info");
+const students = []
+
 
 formEl.addEventListener("submit",(event)=>{
     event.preventDefault(); ///prevent default refresh
@@ -8,5 +11,24 @@ formEl.addEventListener("submit",(event)=>{
     const lastName = event.target.lname.value;
     const grade = event.target.grade.value;
 
-    result.innerHTML=firstName+" "+lastName+" "+grade
-})
+    if(students.find(el=>el.fname==firstName&&el.lname==lastName)){
+        return;
+    }
+
+    students.push(
+        {
+            fname:firstName,
+            lname:lastName,
+            grade:grade
+        }
+    )
+
+tableEl.innerHTML +=`
+<tr>
+<td>${firstName}</td>
+<td>${lastName}</td>
+<td>${grade}</td>
+</tr>
+`
+
+}) 
